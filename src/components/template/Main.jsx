b4f53ecc1f932965bb666f58f56b556e2a3a703d
Carllux <carlos.vinicius-@hotmail.com>
2022-11-28 20:10:05 -0300

    Criando componentes principais e começando estilização

---
+++ src/components/template/Main.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header";
+import React from "react";
+import './Main.css'
+
+export default props =>
+<React.Fragment>
+    <Header />
+    <main className="content">
+        Conteúdo
+    </main>
+</React.Fragment>
---
+++ src/components/template/Nav.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import './Nav.css'
+
+export default props =>
+ <aside className="nav"></aside>
